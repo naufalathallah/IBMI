737a4080aaf617dddbefe91105d9edbbe77a28a6
Naufal Athallah Iwel <naufal@klinikpintar.id>
2023-07-02 11:06:35 +0700

    feat: history page ui

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:ibmi/widgets/info_card.dart';
 
-class HistoryPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _HistoryPageState();
-  }
-}
+class HistoryPage extends StatelessWidget {
+  double? _deviceHeight, _deviceWidth;
 
-class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return CupertinoPageScaffold(
-      child: Container(
-        color: Colors.green,
+      child: _dataCard(),
+    );
+  }
+
+  Widget _dataCard() {
+    return Center(
+      child: InfoCard(
+        height: _deviceHeight! * 0.30,
+        width: _deviceWidth! * 0.75,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _statusText("Normal"),
+            _dateText("05/12/22"),
+            _bmiText("26.98"),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _statusText(String _status) {
+    return Text(
+      _status,
+      style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _dateText(String _date) {
+    return Text(
+      _date,
+      style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _bmiText(String _bmi) {
+    return Text(
+      _bmi,
+      style: const TextStyle(fontSize: 60, fontWeight: FontWeight.w600),
     );
   }
 }
